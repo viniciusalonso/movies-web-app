f2db0269084b07bb1f38fe7382f88ed4eb09a1ca
Vinícius A <vba321@hotmail.com>
2020-01-19 21:21:40 -0300

    Implementing basic listage of upcoming movies

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
+require_relative './services/movie/upcoming.rb'
+require 'dotenv/load'
+
+base_url = ENV['TMDB_API_BASE_URL']
+api_key = ENV['TMDB_API_KEY']
+root_app = ENV['ROOT_APP']
 
 get '/' do
-  erb :index
+  service = Services::Movie::Upcoming.new(base_url, api_key)
+  erb :index, locals: { movies: service.get  }
 end
--- spec/integration/list_page_spec.rb
+++ spec/integration/list_page_spec.rb
@@ -1,12 +1,34 @@
 require 'sinatra_helper'
-require_relative  "../../app/app.rb"
-
 
 describe "Upcoming movies app" do
   include Rack::Test::Methods
 
   def app
     Sinatra::Application
+  end
+
+  let(:movies) do
+    [
+      ::Presenters::Movie.new({
+        "poster_path" => "/y95lQLnuNKdPAzw9F9Ab8kJ80c3.jpg",
+        "id" => 38700,
+        "genre_ids" => [
+          28,
+          80,
+          53
+        ],
+        "title" => "Bad Boys for Life",
+        "release_date" => "2020-01-15"
+      }),
+    ]
+  end
+
+  before do
+    ENV['TMDB_API_KEY'] = 'abc123'
+    ENV['TMDB_API_BASE_URL'] = "https://api.themoviedb.org/3/"
+
+    allow_any_instance_of(::Services::Movie::Upcoming).to receive(:get)
+      .and_return(movies)
   end
 
   describe 'GET /' do
@@ -21,6 +43,15 @@ describe "Upcoming movies app" do
 
       expect(last_response.content_type).to eq('text/html;charset=utf-8')
     end
+
+    it 'render movies list' do
+      get '/'
+
+      expect(last_response.body).to include("Bad Boys for Life")
+      expect(last_response.body).to include("2020/01/15")
+      expect(last_response.body).to include("/y95lQLnuNKdPAzw9F9Ab8kJ80c3.jpg")
+    end
+
   end
 
 end
